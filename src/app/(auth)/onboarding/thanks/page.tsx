9d54d63155bddd45f059e6d50395a1f8743a882c
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-06-11 20:18:08 +0530

    thanku page done

---
+++ src/app/(auth)/onboarding/thanks/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='px-6 py-4 h-screen'>
+        <div className=' flex h-[90%] flex-col justify-center'>
+            <p className="bg-clip-text text-transparent w-[55%] bg-gradient-to-l from-[#E54D60] to-[#A342FF] text-[50px] font-bold leading-[63px] ">Thanks</p>
+            <p className="text-gray-900 text-xl font-bold  leading-[25.20px]">for signing up...</p>
+            <p className=" text-neutral-500 text-[15px] font-normal w-[70%] mt-[12px] leading-tight">Start sharing everything you are, in one simple link.</p>
+        </div>
+        <div className="w-[328px] h-[54px] bg-gradient-to-l from-rose-500 to-purple-500 rounded-[14px] justify-center items-center gap-2.5 inline-flex">
+            <div className="text-white text-base font-semibold ">Continue </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
