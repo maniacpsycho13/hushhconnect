b8bf86a3115aca644fa083b189481e2b89e6995c
maniacpsycho13 <aryansingh98875@gmail.com>
2024-06-12 02:11:13 +0530

    profile page done

--- public/profilePage/index.ts
+++ public/profilePage/index.ts
@@ -1,16 +1,15 @@
 import find from "./find.svg"
 import user from "./user.svg"
 import hushhprofile from "./hushhprofile.svg"
-import Group1 from "./Group1.svg"
-import Group2 from "./Group2.svg"
-import Group3 from "./Group3.svg"
-import Group4 from "./Group4.svg"
-import Group5 from "./Group5.svg"
 import name from "./name.svg"
 import arrowcircle from "./arrowcircle.svg"
 import hushhCard from "./hushhCard.svg"
 import shareicon from "./shareicon.svg"
-
+import instagram from "./instagram.svg"
+import twitter from "./x.svg"
+import linkedin from "./linkedin.svg"
+import facebook from "./facebook.svg"
+import youtube from "./youtube.svg"
 import homepage from "./HomePage.svg"
 
 import backicon from "./backicon.svg"
@@ -33,11 +32,11 @@ export {
     find,
     user,
     hushhprofile,
-    Group1,
-    Group2,
-    Group3,
-    Group4,
-    Group5,
+    instagram,
+    twitter,
+    linkedin,
+    facebook,
+    youtube,
     name,
     arrowcircle,
     hushhCard,
---
+++ src/components/ProfileCard/ProflieCard.tsx
@@ -0,0 +1,171 @@
+"use client"
+import Image from 'next/image'
+import React, { useState } from 'react'
+import {arrowcircle, find, hushhCard, hushhprofile, name, shareicon, user} from "@/../public/profile"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { profileTabs } from '@/Data/ProfileData'
+import { UserDetails } from '@/app/(root)/Profile/page'
+import Link from 'next/link'
+import { facebook, instagram, linkedin, twitter, youtube } from '../../../public/profilePage'
+
+const ProfileCard = (props:UserDetails) => {
+
+    const [showCard, setShowCard] = useState<boolean>(false);
+    const handleToggle = () => {
+        setShowCard(!showCard);
+    };
+    const platformIcons:any = {
+        facebook,
+        instagram,
+        linkedin,
+        twitter,
+        youtube,
+      };
+
+  return (
+    <div>
+       <div className='bg-white px-6 py-4'>
+                <div className='flex justify-between'>
+                <div className="bg-clip-text text-transparent bg-gradient-to-l from-[#E54D60]  to-[#A342FF] text-xl font-semibold ">Hushh Connect</div>
+                    <div className='flex gap-4'>
+                        <Image src={user} alt="user" />
+                        <Image src={find} alt="find" />
+                    </div>
+                </div>
+
+                <div className='mt-4'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <Image src={props.profilePic || hushhprofile} alt="profile" width={108.645} height={108.645} className="w-[108.645px] h-[108.645px] rounded-full" />
+                        <div className='flex flex-col justify-center items-center'>
+                            <h2 className='text-[18px] text-black font-[600] inline'>{props.name}{props.lastname}</h2>
+                            <p onClick={handleToggle} className='cursor-pointer text-[12px] font-[400] mx-auto bg-clip-text text-transparent bg-gradient-to-l from-[#E54D60] to-[#A342FF]'>view hushh card</p>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-evenly my-4'>
+                        {props.socialmedia?.map((item:any) => (
+                            <Link href={item.url} key={item.platform}><Image src={platformIcons[item.platform]} width={26} height={26} alt="Group1" /></Link>
+                        ))}
+                       {/* <Link href={props.socialmedia?.[0].url}><Image src={instagram} alt="Group1" /></Link>
+                       <Link href={''}><Image src={twitter} width={26} height={26} alt="Group1" /></Link>
+                       <Link href={''}><Image src={facebook} alt="Group1" /></Link>
+                       <Link href={''}><Image src={youtube} alt="Group1" /></Link>
+                       <Link href={''}><Image src={linkedin} alt="Group1" /></Link> */}
+                        
+                    </div>
+
+                    <div className='bg-[#EFEFEF] rounded-xl p-4 mx-[5px]'>
+                        <p className='text-center text-[#171717] text-[12px] font-medium'>{props.bio}</p>
+                    </div>
+
+                    {/* <div className='my-4 text-[12px] font-medium text-black flex justify-between'>
+                       <div className='py-[6px] px-[24px] rounded-[4px] addcolor text-white'>Thread</div>
+                       <div className='py-[6px] px-[24px] bg-[#EFEFEF] rounded-[4px]'>Products</div>
+                       <div className='py-[6px] px-[24px] bg-[#EFEFEF] rounded-[4px]'>Schedule Meet</div>
+                     </div> */}
+
+{/* ------------------------------------------------------------------------------------------------------------ */}
+<div className='mt-[16.14px]'>
+        <Tabs defaultValue='threads' className='w-full'>
+           <TabsList className='flex bg-transparent gap-[10px]' >
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                {/* <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className='object-contain max-sm:hidden'
+                /> */}
+                <p className=''>{tab.label}</p>
+
+                
+              </TabsTrigger>
+            ))}
+          </TabsList> 
+          
+            <TabsContent
+              key={`content-threads`}
+              value="threads"
+              className='w-full text-light-1'
+            >
+              {/* <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType='User'
+              />  */}
+            </TabsContent>
+
+            <TabsContent
+              key={`content-Product`}
+              value="Product"
+              className='w-full text-light-1'
+            >
+              {/* <div><ProductsTab 
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType='User'></ProductsTab></div> */}
+
+            </TabsContent>
+
+            <TabsContent
+              key={`content-Meeting`}
+              value="Meeting"
+              className='w-full text-light-1'
+            >
+              {/* <Payment></Payment> */}
+              {/* <div className="text-white my-8">
+                <div>Note:</div>
+                <br />
+                <ul className="list-disc ml-4"> 
+                  <li>Please Add your email in Guest Section</li>
+                </ul>
+              </div>
+              <div></div> */}
+              
+            </TabsContent>
+
+
+        </Tabs>
+      </div>
+
+{/* ----------------------------------------------------------------------------------------------------------------------- */}
+
+
+
+
+
+                </div>
+            </div>
+
+            <div className={`fixed inset-0 bg-black bg-opacity-80 backdrop-blur-[7.20px] flex items-end transition-transform duration-300 ${showCard ? 'translate-y-0' : 'translate-y-full'}`}>
+                <div className='w-full shadow-lg rounded-t-lg'>
+                    <div className='px-[14px]'>
+                        <div className="w-full h-2 bg-zinc-300 rounded-tl-lg rounded-tr-lg" />
+                    </div>
+                    <div className='px-[13px] py-[14px] border-[1px] border-hidden bg-white rounded-tl-lg rounded-tr-lg h-[432px]'>
+                        <div className='flex justify-end'>
+                            <Image src={arrowcircle} alt="Group1" onClick={handleToggle} />
+                        </div>
+                        <div className='flex justify-center items-center mt-[-0.8rem] flex-col'>
+                            <Image src={props.profilePic || hushhprofile} width={68.64} height={68.64} alt="profile" className="w-[68.64px] h-[68.64px] rounded-full" />
+                            <p className='text-black text-lg font-semibold leading-[33.29px]'>{props.name}{props.lastname}</p>
+                        </div>
+                        <div className='relative'>
+                            <Image src={hushhCard} alt="card" className="w-full  relative" />
+                            <div className='absolute top-[16px] left-[24px] flex flex-col'>
+                                <p className="text-white text-2xl font-normal font-['Figtree']">{props.name}{props.lastname}</p>
+                                <p className="text-white text-[8.85px] font-medium font-['Figtree']">{props.email}</p>
+                            </div>
+                            <div className='absolute top-[24px] right-[26px]'>
+                                <Image src={shareicon} alt="card" className="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+    </div>
+  )
+}
+
+export default ProfileCard
