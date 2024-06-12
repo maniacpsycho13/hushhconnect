af15a51f26859c3c9c94a5653b1c53f432f80a80
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-06-13 03:47:22 +0530

    Update LowerNavbar.tsx

--- src/components/LowerNavbar/LowerNavbar.tsx
+++ src/components/LowerNavbar/LowerNavbar.tsx
@@ -19,7 +19,7 @@ const LowerNavbar = ( { id } : { id : string} ) => {
       <div className={`w-full flex flex-col fixed bottom-0 z-10`}>
         <Image src={lowerNav2} alt='nav' className='relative shadow-xl w-full'></Image>
         <div className='flex absolute  w-full mt-[1.7rem] px-6 z-10'>
-          <Link href={'/home'}> <Image src={homeicon} alt='nav' className=''></Image></Link>
+          <Link href={'/home/thread'}> <Image src={homeicon} alt='nav' className=''></Image></Link>
 
           <Image src={bell} alt='nav' className='ml-[16%]'></Image>
           <Image src={community} alt='nav' className='ml-[40%]'></Image>
