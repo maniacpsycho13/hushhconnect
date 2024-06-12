c35875dcf56f2a4f192b9621d4b4154d5f33b6a8
maniacpsycho13 <aryansingh98875@gmail.com>
2024-06-12 18:29:02 +0530

    login form updated

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,11 @@
 import SignInEmail from "@/components/form/AuthForm/SignIn/SignInEmail";
+import OauthSignIn from "@/components/form/AuthForm/SignIn/SignInGoogle";
 import SignInPage from "@/components/form/AuthForm/SignIn/SigninForm";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignInEmail />;
+  return<div className="border border-blue-500">
+   <SignInEmail />;
+   <OauthSignIn />
+  </div>
 }
