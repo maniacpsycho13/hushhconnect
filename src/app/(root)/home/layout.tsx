583cbd9442967e55d2c40f1c95900fe14c272dba
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-06-13 01:44:39 +0530

    check it

---
+++ src/app/(root)/home/layout.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React, { Children } from 'react'
+
+const layout = () => {
+  return (
+    <div >
+        <div className='px-6'>
+        <div className='flex justify-between items-center'>
+        <div className="bg-clip-text text-transparent bg-gradient-to-l from-[#E54D60]  to-[#A342FF] text-xl font-semibold ">Hushh Connect</div> 
+          <div className='flex gap-[26px] pt-[6px]'>
+            <Link href={'thread'}><div className=" text-gray-500 text-sm font-medium  leading-[22.91px] ">Thread</div></Link>
+            <Link href={'product'}><div className=" text-gray-500 text-sm font-medium  leading-[22.91px]">Product</div></Link>
+          </div>
+        </div>
+        </div>
+    <div>{Children}</div>
+    </div>
+  )
+}
+
+export default layout
