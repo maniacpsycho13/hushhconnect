aa6c135e73bfea66c0de9b4126c3d8732bd1fe5e
maniacpsycho13 <aryansingh98875@gmail.com>
2024-06-13 22:19:07 +0530

    doing check

--- src/app/(root)/home/layout.tsx
+++ src/app/(root)/home/layout.tsx
@@ -6,8 +6,8 @@ const layout = ({ children }: { children: React.ReactNode }) => {
         <div className='flex justify-between items-center'>
         <div className="bg-clip-text text-transparent bg-gradient-to-l from-[#E54D60]  to-[#A342FF] text-xl font-semibold ">Hushh Connect</div> 
           <div className='flex gap-[26px] pt-[6px]'>
-            <Link href={'home/thread'}><div className=" text-gray-500 text-sm font-medium  leading-[22.91px] ">Thread</div></Link>
-            <Link href={'home/product'}><div className=" text-gray-500 text-sm font-medium  leading-[22.91px]">Product</div></Link>
+            <Link href={'thread'}><div className=" text-gray-500 text-sm font-medium  leading-[22.91px] ">Thread</div></Link>
+            <Link href={'product'}><div className=" text-gray-500 text-sm font-medium  leading-[22.91px]">Product</div></Link>
           </div>
         </div>
         </div>
