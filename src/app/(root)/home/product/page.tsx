dc0737abff5d12f6a866080bc75bbfa4dd833665
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-06-18 00:20:37 +0530

    Product done

--- src/app/(root)/home/product/page.tsx
+++ src/app/(root)/home/product/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Post from '@/components/Post/Post';
+import { PostsSkeleton } from '@/components/Post/Skeletons';
+import ProductCard from '@/components/ProductCard/ProductCard';
+import { fetchProductsByUserId } from '@/lib/Actions/product.action';
+import React, { Suspense } from 'react'
 
-const page = () => {
+export default async function page({ params }: { params: { id: string } }) {
+
+  const products=await fetchProductsByUserId(params.id);
+
+
   return (
-    <div>page</div>
+    <div className='grid grid-cols-2 pb-32 pt-[25px] px-4'>
+        <Suspense fallback={<PostsSkeleton/>}>
+        {products?.map((post:any) => (
+            <ProductCard key={post.id} fileUrl={post.fileUrl} title={post.title} price={post.price} currency={post.currency} link={post.link} />
+            ))}
+        </Suspense>
+    </div>
   )
 }
-
-export default page
