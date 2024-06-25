cf0bed3e8ca828b2f7dc8d36d4258f3f9bb130c7
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-06-25 21:59:11 +0530

    activity page complted

---
+++ src/app/(root)/activity/page.tsx
@@ -0,0 +1,12 @@
+import Activity from '@/components/Activity/Activity'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Activity/>
+    </div>
+  )
+}
+
+export default page
