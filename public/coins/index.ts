3e9df5a92ab25f4e5206b9a47a3201f7af1ed512
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-06-25 22:50:26 +0530

    achievement completed

--- public/coins/index.ts
+++ public/coins/index.ts
@@ -4,6 +4,17 @@ import medal from "./medal.svg"
 import gift from "./gift.svg"
 import copyicon from "./copyicon.svg"
 import referal from "./referal.svg"
+import crown from "./crown.svg"
+import P1 from "./P1.svg"
+import P2 from "./P2.svg"
+import P3 from "./P3.svg"
+import P4 from "./P4.svg"
+import P5 from "./P5.svg"
+import P6 from "./P6.svg"
+import P7 from "./P7.svg"
+import P8 from "./P8.svg"
+import P9 from "./P9.svg"
+import P10 from "./P10.svg"
 
 export{
     whitebackicon,
@@ -11,5 +22,16 @@ export{
     medal,
     gift,
     copyicon,
-    referal
+    referal,
+    crown,
+    P1,
+    P2,
+    P3,
+    P4,
+    P5,
+    P6,
+    P7,
+    P8,
+    P9,
+    P10
 }
