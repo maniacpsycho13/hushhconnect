cfeb28c20642c78d33a573afde06cd161cb39974
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-07-03 22:03:23 +0530

    setting completed

--- src/app/(root)/settings/page.tsx
+++ src/app/(root)/settings/page.tsx
@@ -2,25 +2,40 @@ import Image from 'next/image'
 import React from 'react'
 import { NextIcon, SettingIcon1, SettingIcon2, SettingIcon3, SettingIcon4, SettingIcon5, SettingIcon6, SettingIcon7, SettingIcon8 } from '../../../../public/Settings'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const page = () => {
   return (
     <div>
         <div className='px-6 py-4 bg-gray-100 h-screen w-screen'>
-            <h2 className='text-xl font-semibold bg-clip-text text-transparent bg-gradient-to-l from-[#E54D60] to-[#A342FF] w-[20%]'>Profile</h2>
-            <div>
-                <div className="text-indigo-950 text-xs font-normal mt-[35px] uppercase leading-[14.37px] tracking-tight">Allow Assets to access</div>
-                <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
-                    <div className='flex gap-[7.66px]'>
-                        <Image src={SettingIcon1} alt='Manage'></Image>
-                        <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Manage your connections</div>
-                    </div>
-                    <div >
-                        <Image src={NextIcon} alt='icon'></Image>
-                    </div>
+            <div className='flex justify-between'>
+                <h2 className='text-xl font-semibold bg-clip-text text-transparent bg-gradient-to-l from-[#E54D60] to-[#A342FF]'>Profile</h2>
+                <div className='flex  flex-col items-center'>
+                            {/* <Image src={SettingIcon8} alt='Manage'></Image> */}
+                                <div>
+                                    <SignedIn>
+                                    <UserButton/>
+                                    </SignedIn> 
+                                </div> 
+                                <div className='font-semibold text-black text-sm'>Logout</div>
                 </div>
+                </div>
+            <div>
+                <div className="text-indigo-950 text-xs font-normal mt-[12px] uppercase leading-[14.37px] tracking-tight">Allow Assets to access</div>
+                <Link href={'/underprogress'}>
+                    <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
+                        <div className='flex gap-[7.66px]'>
+                            <Image src={SettingIcon1} alt='Manage'></Image>
+                            <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Manage your connections</div>
+                        </div>
+                        <div >
+                            <Image src={NextIcon} alt='icon'></Image>
+                        </div>
+                    </div>
+                </Link>
 
                 <div className="text-indigo-950 text-xs font-normal mt-[23px] uppercase leading-[14.37px] tracking-tight">General</div>
+                <Link href={'/underprogress'}>
                 <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
                     <div className='flex gap-[7.66px]'>
                         <Image src={SettingIcon2} alt='Manage'></Image>
@@ -30,50 +45,60 @@ const page = () => {
                         <Image src={NextIcon} alt='icon'></Image>
                     </div>
                 </div>
+                </Link>
 
-                <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
-                    <div className='flex gap-[7.66px]'>
-                        <Image src={SettingIcon3} alt='Manage'></Image>
-                        <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">App appearance</div>
+                <Link href={'/underprogress'}>
+                    <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
+                        <div className='flex gap-[7.66px]'>
+                            <Image src={SettingIcon3} alt='Manage'></Image>
+                            <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">App appearance</div>
+                        </div>
+                        <div >
+                            <Image src={NextIcon} alt='icon'></Image>
+                        </div>
                     </div>
-                    <div >
-                        <Image src={NextIcon} alt='icon'></Image>
-                    </div>
-                </div>
+                </Link>
 
-                <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
-                    <div className='flex gap-[7.66px]'>
-                        <Image src={SettingIcon4} alt='Manage'></Image>
-                        <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Notification</div>
+                <Link href={'/underprogress'}>
+                    <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
+                        <div className='flex gap-[7.66px]'>
+                            <Image src={SettingIcon4} alt='Manage'></Image>
+                            <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Notification</div>
+                        </div>
+                        <div >
+                            <Image src={NextIcon} alt='icon'></Image>
+                        </div>
                     </div>
-                    <div >
-                        <Image src={NextIcon} alt='icon'></Image>
-                    </div>
-                </div>
+                </Link>
 
                 <div className="text-indigo-950 text-xs font-normal mt-[23px] uppercase leading-[14.37px] tracking-tight">wallet</div>
-                <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
-                    <div className='flex gap-[7.66px]'>
-                        <Image src={SettingIcon5} alt='Manage'></Image>
-                        <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Wallet</div>
+                <Link href={'/underprogress'}>
+                    <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
+                        <div className='flex gap-[7.66px]'>
+                            <Image src={SettingIcon5} alt='Manage'></Image>
+                            <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Wallet</div>
+                        </div>
+                        <div >
+                            <Image src={NextIcon} alt='icon'></Image>
+                        </div>
                     </div>
-                    <div >
-                        <Image src={NextIcon} alt='icon'></Image>
-                    </div>
-                </div>
+                </Link>
 
-                <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
-                    <div className='flex gap-[7.66px]'>
-                        <Image src={SettingIcon6} alt='Manage'></Image>
-                        <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Data management</div>
+                <Link href={'/underprogress'}>
+                    <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
+                        <div className='flex gap-[7.66px]'>
+                            <Image src={SettingIcon6} alt='Manage'></Image>
+                            <div className=" text-indigo-950 text-base font-normal  leading-[22.99px]">Data management</div>
+                        </div>
+                        <div >
+                            <Image src={NextIcon} alt='icon'></Image>
+                        </div>
                     </div>
-                    <div >
-                        <Image src={NextIcon} alt='icon'></Image>
-                    </div>
-                </div>
+                </Link>
 
                 <div className="text-indigo-950 text-xs font-normal  mt-[23px] uppercase leading-[14.37px] tracking-tight">help</div>
 
+                <Link href={'/underprogress'}>
                 <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between'>
                     <div className='flex gap-[7.66px]'>
                         <Image src={SettingIcon7} alt='Manage'></Image>
@@ -83,8 +108,9 @@ const page = () => {
                         <Image src={NextIcon} alt='icon'></Image>
                     </div>
                 </div>
+                </Link>
 
-                <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between '>
+                {/* <div className='mt-[8.03px] h-[45.98px] w-full pl-[15.33px] bg-white rounded-[9.58px] pr-[15px] items-center inline-flex justify-between '>
                     <div className='flex gap-[7.66px]'>
                         <Image src={SettingIcon8} alt='Manage'></Image>
                             <SignedIn>
@@ -94,7 +120,7 @@ const page = () => {
                     <div >
                         <Image src={NextIcon} alt='icon'></Image>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
         <div className='bg-gray-100 w-full h-28'>
---
+++ src/app/(root)/underprogress/page.tsx
@@ -0,0 +1,12 @@
+import Development from '@/components/UnderDevelopment/Development'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='px-6 py-4 h-screen flex justify-center items-center'>
+        <Development/>
+    </div>
+  )
+}
+
+export default page
