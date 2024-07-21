3dcf6bc5334a393e1a6298a17baab8f76779301d
Anmolsingh0713 <158687317+Anmolsingh0713@users.noreply.github.com>
2024-07-21 06:51:57 +0530

    connect all onboarding pages and template page updated

--- src/app/(auth)/onboarding/template/page.tsx
+++ src/app/(auth)/onboarding/template/page.tsx
@@ -1,20 +1,24 @@
+import Template from '@/components/Template/Template'
 import Link from 'next/link'
 import React from 'react'
+import { fetchProfile } from "@/lib/Actions/user.action";
+import { auth } from "@clerk/nextjs/server";
 
-const page = () => {
+export default async function page ()  {
+  const session = await auth();
+  if (!session || !session.userId) {
+    return null;
+  }
+  const profile=await fetchProfile(session.userId);
+  
+  if(!profile)return null;
   return (
     <div className='px-6 py-4'>
         <div className="text-center text-gray-900 text-xl font-bold mt-[24px] ">Templates</div>
 
         <div className="text-center text-neutral-500 text-sm font-normal mt-[8px] leading-tight">Add your own content below. You can also further customize your links and appearance later.</div>
-
-        <div className="mt-[6rem]">
-            <Link href={'social'}>
-              <div  className="h-14  py-[17px] bg-gradient-to-l from-rose-500 to-purple-500 rounded-[10px] justify-center items-center gap-2.5 inline-flex w-full text-center text-white text-base font-semibold leading-tight">Continue</div>
-            </Link>
-        </div>
+        <Template id={session.userId} user={profile}></Template>
     </div>
-  )
+  );
 }
 
-export default page
